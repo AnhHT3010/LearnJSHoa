b8df31dd2951a12d9ceaf10b3aeea1947d2f2c04
AnhHT3010 <trunganhhoang2001@gmail.com>
2025-06-01 09:16:12 +0700

    first commit

---
+++ condition.js
@@ -0,0 +1,13 @@
+/*
+Chương 2: Cấu trúc điều kiện
+- Giá trị `boolean` => true và false
+- Toán tử so sánh: >, >=, <, <=, ==, !=
+- Toán tử logic: &&, ||
+- Triển khai cấu trúc điều kiện `if, if-else, if-[else if]-else`
+  syntax: 
+  if (điều kiện) {
+    // code thực thi nếu điều kiện đúng
+  }else {
+    // code thực thi nếu điều kiện sai
+  }
+*/
